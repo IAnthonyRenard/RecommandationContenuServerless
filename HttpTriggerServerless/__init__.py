450bd8949da4d74133d8a6e245de772571e2f58f
IAnthonyRenard <contact.anthony.renard@gmail.com>
2023-07-21 11:42:50 +0200

    Test chargement fichier csv

--- HttpTriggerServerless/__init__.py
+++ HttpTriggerServerless/__init__.py
@@ -1,7 +1,15 @@
 import logging
 import json
 import azure.functions as func
+from azure.storage.blob import BlobClient
+import pandas as pd
+from io import StringIO
 
+sas_url = "https://conteneur3.blob.core.windows.net/conteneur3/clicks2.csv"
+blob_client = BlobClient.from_blob_url(sas_url)
+blob_data = blob_client.download_blob()
+df = pd.read_csv(StringIO(blob_data.content_as_text()))
+print(df)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
@@ -16,6 +24,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('userID')
 
     if name:
+        #chargement du fichier d'interactions
+        
+        
         recommendations = ['Article 1', 'Article 2', 'Article 3', 'Article 4', 'Article 5']
         response_data = {'userID': name,'recommendations': recommendations[:5]}
         response_body = json.dumps(response_data)
@@ -26,26 +37,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
-
-
-'''def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    # Récupérer les paramètres de la requête
-    id = req.params.get('id')
-    rec_type = req.params.get('type')
-    # Effectuer ici la logique nécessaire pour obtenir la liste d'articles recommandés
-    # en fonction de l'ID utilisateur et du type de recommandation
-    # Exemple factice : renvoyer une liste de 5 articles recommandés statiques
-    recommended_articles = ['Article 1', 'Article 2', 'Article 3', 'Article 4', 'Article 5']
-    # Générer la réponse JSON
-    response_data = {
-        'user_id': id,
-        'recommendations': recommended_articles[:5]  # Prendre les 5 premiers articles recommandés
-    }
-    response_body = json.dumps(response_data)
-    # Renvoyer la réponse HTTP
-    return func.HttpResponse(
-        response_body,
-        mimetype='application/json',
-        status_code=200
-    )'''                                                                 
+                                                                
