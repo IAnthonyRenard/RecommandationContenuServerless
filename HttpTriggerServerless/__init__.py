d48556372695550227227884f439c2e545b7025b
IAnthonyRenard <contact.anthony.renard@gmail.com>
2023-07-24 09:02:43 +0200

    test

--- HttpTriggerServerless/__init__.py
+++ HttpTriggerServerless/__init__.py
@@ -10,19 +10,18 @@ import pickle
 ''' *******Chargement des fichiers********'''
 
 #1. chargement du fichier embedding
-#sas_url = "https://conteneur3.blob.core.windows.net/conteneur3/clicks2.csv"
 sas_url= "https://conteneur3.blob.core.windows.net/conteneur3/df_embeddings_inter.csv"
 blob_client = BlobClient.from_blob_url(sas_url)
 blob_data = blob_client.download_blob()
 df_embeddings = pd.read_csv(StringIO(blob_data.content_as_text()), index_col=0)
-'''
+
 #2. chargement du fichier interactions
 sas_url = "https://conteneur3.blob.core.windows.net/conteneur3/clicks2.csv"
 blob_client = BlobClient.from_blob_url(sas_url)
 blob_data = blob_client.download_blob()
 df_clicks = pd.read_csv(StringIO(blob_data.content_as_text()))
 
-#3. chargement du modèle
+'''#3. chargement du modèle
 sas_url = "https://conteneur3.blob.core.windows.net/conteneur3/recommender.model"
 blob_client = BlobClient.from_blob_url(sas_url)
 blob_data = blob_client.download_blob()
@@ -55,7 +54,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('userID')
 
     if name:
-        print("la taille de la dataframe embedding est :", df_embeddings.shape)
+        print("La taille de la dataframe embedding est :", df_embeddings.shape)
+        print("La taille de la dataframe intéractions est :", df_clicks.shape)
+        
+        
         print("lancement recoms")
         recommendations = ['Article 1', 'Article 2', 'Article 3', 'Article 4', 'Article 5']
         response_data = {'userID': name,'recommendations': recommendations[:5]}
