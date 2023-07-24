c07ecdd494837cc4d86a80f49a624ae182e4a899
IAnthonyRenard <contact.anthony.renard@gmail.com>
2023-07-24 11:29:08 +0200

    test

--- HttpTriggerServerless/__init__.py
+++ HttpTriggerServerless/__init__.py
@@ -17,10 +17,10 @@ blob_data = blob_client.download_blob()
 df_embeddings = pd.read_csv(StringIO(blob_data.content_as_text()), index_col=0)
 
 #2. chargement du fichier interactions
-'''sas_url = "https://conteneur3.blob.core.windows.net/conteneur3/clicks2.csv"
+sas_url = "https://conteneur3.blob.core.windows.net/conteneur3/clicks2.csv"
 blob_client = BlobClient.from_blob_url(sas_url)
 blob_data = blob_client.download_blob()
-df_clicks = pd.read_csv(StringIO(blob_data.content_as_text()),chunksize=10 ** 6)'''
+df_clicks = pd.read_csv(StringIO(blob_data.content_as_text()),chunksize=10 ** 6)
 
 #3. chargement du modèle
 '''sas_url = "https://conteneur3.blob.core.windows.net/conteneur3/recommender.model"
@@ -59,7 +59,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if name:
         print("La taille de la dataframe embedding est :", df_embeddings.shape)
-       
+        print("La taille de la dataframe df_clisks est :", df_clicks.shape)
         
         
         print("lancement recoms")
